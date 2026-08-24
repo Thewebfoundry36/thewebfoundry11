14d6d7660e34fe132406fec1a86e7a8c99affa5a
Thewebfoundry36 <anjithjjiji@gmail.com>
2026-08-24 17:09:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,5 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
-    spa: {
-      enabled: true,
-    },
   },
 });
